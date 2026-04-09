f498831b11c29138c1831859d4f9b93e48084c4a
matheus44244 <mhas2@aluno.ifnmg.edu.br>
2026-04-09 23:14:58 +0000

    feat: pratica funções e métodos de array

--- exercicios/aula06/index.js
+++ exercicios/aula06/index.js
@@ -28,5 +28,7 @@ export function buscarAluno(alunos, nome) {
 
 // Exercício 5 — calcularMedia
 export function calcularMedia(notas) {
-  // escreva seu código aqui
+  if (notas.length === 0) return 0;
+  const soma = notas.reduce((acc, nota) => acc + nota, 0);
+  return soma / notas.length;
 }
